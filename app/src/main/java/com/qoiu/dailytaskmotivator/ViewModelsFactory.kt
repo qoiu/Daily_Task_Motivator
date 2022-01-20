d1f18ecf3d8bb1e6a8e3aaa903b621af85195a53
Nikita <guschinnick@gmail.com>
2022-01-20 17:51:10 +0300

    add shop fragment factory

--- app/src/main/java/com/qoiu/dailytaskmotivator/ViewModelsFactory.kt
+++ app/src/main/java/com/qoiu/dailytaskmotivator/ViewModelsFactory.kt
@@ -2,15 +2,18 @@ package com.qoiu.dailytaskmotivator
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.qoiu.dailytaskmotivator.presentation.MainViewModel
-import com.qoiu.dailytaskmotivator.presentation.MainViewModelFactory
-import com.qoiu.dailytaskmotivator.presentation.TaskModel
-import com.qoiu.dailytaskmotivator.presentation.TaskModelFactory
+import com.qoiu.dailytaskmotivator.presentation.main.MainViewModel
+import com.qoiu.dailytaskmotivator.presentation.main.MainViewModelFactory
+import com.qoiu.dailytaskmotivator.presentation.shop.ShopModel
+import com.qoiu.dailytaskmotivator.presentation.shop.ShopModelFactory
+import com.qoiu.dailytaskmotivator.presentation.task.TaskModel
+import com.qoiu.dailytaskmotivator.presentation.task.TaskModelFactory
 
 class ViewModelsFactory(coreModule: CoreModule) {
     private val map = HashMap<Class<out ViewModel>, ViewModelProvider.Factory>().apply {
         put(MainViewModel::class.java, MainViewModelFactory(coreModule.mainInteractor))
         put(TaskModel::class.java, TaskModelFactory(coreModule.taskInteractor))
+        put(ShopModel::class.java, ShopModelFactory())
     }
 
     fun get(modelClass: Class<out ViewModel>) =
