8f6e1c544434ad6b0e118ee3525aa1b4591e2d2c
Nikita <guschinnick@gmail.com>
2022-01-22 03:21:41 +0300

    remove sharedPref from TaskRepository.kt

--- app/src/main/java/com/qoiu/dailytaskmotivator/data/task/TaskRepository.kt
+++ app/src/main/java/com/qoiu/dailytaskmotivator/data/task/TaskRepository.kt
@@ -1,25 +1,14 @@
 package com.qoiu.dailytaskmotivator.data.task
 
 import com.qoiu.dailytaskmotivator.Mapper
-import com.qoiu.dailytaskmotivator.Read
-import com.qoiu.dailytaskmotivator.Save
 import com.qoiu.dailytaskmotivator.data.AbstractRepository
 import com.qoiu.dailytaskmotivator.data.RealmDataSource
-import com.qoiu.dailytaskmotivator.data.SharedData
 import com.qoiu.dailytaskmotivator.domain.entities.Task
 
 class TaskRepository(
     taskDataSource: RealmDataSource<TaskDb>,
     domainMapper: Mapper.Data<TaskDb, Task>,
-    dbMapper: Mapper.Data<Task,TaskDb>,
-    private val sharedData: SharedData
-) : AbstractRepository<TaskDb, Task>(taskDataSource, domainMapper, dbMapper), Save<Int>,
-    Read<Int> {
+    dbMapper: Mapper.Data<Task,TaskDb>
+) : AbstractRepository<TaskDb, Task>(taskDataSource, domainMapper, dbMapper) {
 
-    override fun save(data: Int) {
-        sharedData.save(data)
-    }
-
-    override fun read(): Int =
-        sharedData.read()
 }
