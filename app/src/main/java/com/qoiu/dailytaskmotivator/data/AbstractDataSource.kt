8c941e881dae7ebd781da542fe7f4d3e5cc25b4a
Nikita <guschinnick@gmail.com>
2022-01-22 03:09:44 +0300

    fix AbstractDataSource.kt

--- app/src/main/java/com/qoiu/dailytaskmotivator/data/AbstractDataSource.kt
+++ app/src/main/java/com/qoiu/dailytaskmotivator/data/AbstractDataSource.kt
@@ -2,7 +2,6 @@ package com.qoiu.dailytaskmotivator.data
 
 import com.qoiu.dailytaskmotivator.RealmProvider
 import com.qoiu.dailytaskmotivator.UpdatableRealm
-import com.qoiu.dailytaskmotivator.data.category.CategoryDb
 import io.realm.RealmAny
 
 abstract class AbstractDataSource<T: UpdatableRealm<T>>(private val realmProvider: RealmProvider): RealmDataSource<T> {
@@ -30,7 +29,7 @@ abstract class AbstractDataSource<T: UpdatableRealm<T>>(private val realmProvide
     override fun remove(data: T) {
         realmProvider.provide().use { realm ->
             realm.executeTransaction {
-                val obj = realm.where(CategoryDb::class.java).equalTo(primaryKey, compareTo(data))
+                val obj = realm.where(realmClass()).equalTo(primaryKey, compareTo(data))
                     .findAll()
                 obj.deleteAllFromRealm()
             }
